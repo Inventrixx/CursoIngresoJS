00bbd9c7c58619f9dc3c1fa1ba9a9be1fc74a50a
celes0000 <anaclc15@hotmail.com>
2017-06-27 21:28:51 -0300

    actividad 3

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,7 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
-{
+{var mensaje= "esto funciona de maravilla"
+    alert("esto funciona de maravila")
 	
 }
 
