b99264f91c004386e201ca825a62677ad0c93884
celes0000 <anaclc15@hotmail.com>
2017-06-30 22:16:09 -0300

    Add files via upload

---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,15 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+*/
+function Mostar()
+{var nombre
+var edad
+	nombre=document.getElementById("elNombre").value;
+	nombre=document.getElementById("laEdad").value;
+	alert("ustedsellama"nombre "y tiene" edad "años")
+	
+
+	
+}
+
---
+++ jsEntradaSalida-7.js
@@ -0,0 +1,50 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostar el resulto por medio de "ALERT"
+ej.: "la Resta es 750"*/ 
+function sumar()
+{var numerouno
+    var numerodos
+	numerouno= document.getElementById("numeroUno").value
+    numerodos= document.getElementById("numeroDos").value
+    numerouno= parseInt(numerouno); 
+    numerodos= parseInt(numerodos);
+  resultado= numerouno + numerodos
+  alert("la suma es " + resultado)
+}
+
+function restar()
+{var numerouno
+    var numerodos
+	numerouno= document.getElementById("numeroUno").value
+    numerodos= document.getElementById("numeroDos").value
+    numerouno= parseInt(numerouno); 
+    numerodos= parseInt(numerodos);
+  resultado= numerouno - numerodos
+  alert("la resta es " + resultado)
+}
+
+function multiplicar()
+{var numerouno
+    var numerodos
+	numerouno= document.getElementById("numeroUno").value
+    numerodos= document.getElementById("numeroDos").value
+    numerouno= parseInt(numerouno); 
+    numerodos= parseInt(numerodos);
+  resultado= numerouno * numerodos
+  alert("la multiplicación es " + resultado)
+	
+}
+
+function dividir()
+{var numerouno
+    var numerodos
+	numerouno= document.getElementById("numeroUno").value
+    numerodos= document.getElementById("numeroDos").value
+    numerouno= parseInt(numerouno); 
+    numerodos= parseInt(numerodos);
+  resultado= numerouno / numerodos
+  alert("la división es " + resultado)
+	
+}
+
